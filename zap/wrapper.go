de2d2eca9a2b269b3ae43119fe50b130aca951b5
Soner Eker <sonereker@gmail.com>
2022-02-18 21:09:25 +0300

    Introduce `DiscardRules`

--- zap/wrapper.go
+++ zap/wrapper.go
@@ -1,65 +1,67 @@
 package zap
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+)
 
-type wrapper struct {
-	logger *zap.SugaredLogger
+type Logger struct {
+	log *zap.SugaredLogger
 }
 
-func (w *wrapper) Errorf(template string, err interface{}, args ...interface{}) {
+func (w *Logger) Errorf(template string, err interface{}, args ...interface{}) {
 	if err != nil {
-		w.logger.With(zap.Error(err.(error))).Errorf(template, args...)
+		w.log.With(zap.Error(err.(error))).Errorf(template, args...)
 
 		return
 	}
 
-	w.logger.Errorf(template, args...)
+	w.log.Errorf(template, args...)
 }
 
-func (w *wrapper) Errorw(msg string, err interface{}, keysAndValues ...interface{}) {
+func (w *Logger) Errorw(msg string, err interface{}, keysAndValues ...interface{}) {
 	if err != nil {
-		w.logger.With(zap.Error(err.(error))).Errorw(msg, keysAndValues...)
+		w.log.With(zap.Error(err.(error))).Errorw(msg, keysAndValues...)
 
 		return
 	}
 
-	w.logger.Errorw(msg, keysAndValues...)
+	w.log.Errorw(msg, keysAndValues...)
 }
 
-func (w *wrapper) Error(err interface{}, args ...interface{}) {
+func (w *Logger) Error(err interface{}, args ...interface{}) {
 	if err != nil {
-		w.logger.With(zap.Error(err.(error))).Errorw("Error", args...)
+		w.log.With(zap.Error(err.(error))).Errorw("Error", args...)
 
 		return
 	}
 
-	w.logger.Errorw("Error", args...)
+	w.log.Errorw("Error", args...)
 }
 
-func (w *wrapper) Infof(template string, args ...interface{}) {
-	w.logger.Infof(template, args...)
+func (w *Logger) Infof(template string, args ...interface{}) {
+	w.log.Infof(template, args...)
 }
 
-func (w *wrapper) Infow(msg string, keysAndValues ...interface{}) {
-	w.logger.Infow(msg, keysAndValues...)
+func (w *Logger) Infow(msg string, keysAndValues ...interface{}) {
+	w.log.Infow(msg, keysAndValues...)
 }
 
-func (w *wrapper) Info(args ...interface{}) {
-	w.logger.Info(args...)
+func (w *Logger) Info(args ...interface{}) {
+	w.log.Info(args...)
 }
 
-func (w *wrapper) Debugf(template string, args ...interface{}) {
-	w.logger.Debugf(template, args...)
+func (w *Logger) Debugf(template string, args ...interface{}) {
+	w.log.Debugf(template, args...)
 }
 
-func (w *wrapper) Debugw(msg string, keysAndValues ...interface{}) {
-	w.logger.Debugw(msg, keysAndValues...)
+func (w *Logger) Debugw(msg string, keysAndValues ...interface{}) {
+	w.log.Debugw(msg, keysAndValues...)
 }
 
-func (w *wrapper) Debug(args ...interface{}) {
-	w.logger.Debug(args...)
+func (w *Logger) Debug(args ...interface{}) {
+	w.log.Debug(args...)
 }
 
-func (w *wrapper) Sync() {
-	_ = w.logger.Sync()
+func (w *Logger) Sync() {
+	_ = w.log.Sync()
 }
